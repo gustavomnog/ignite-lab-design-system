6847a9100199a9739623aa9e23b050bb64fa6a1f
Gustavo M Nogueira <gustavo.mnogueira95@gmail.com>
2022-10-13 19:36:05 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './styles/global.css'
+
+export const App = () => {
+
+  return (
+    <>
+    <h1 className='text-2xl'>Hello World</h1>
+
+    <button className='bg-cyan-500 text-gray-400 hover:bg-gray-100' >
+      Enviar
+    </button>
+
+    
+    </>
+  )
+}
+
+export default App
